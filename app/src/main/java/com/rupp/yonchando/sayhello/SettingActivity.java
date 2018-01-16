3997216a7a9573301f0586680d709a575c0542d9
Yon Chando <von.chando@gmail.com>
2018-01-16 16:14:00 +0700

    setting

--- app/src/main/java/com/rupp/yonchando/sayhello/SettingActivity.java
+++ app/src/main/java/com/rupp/yonchando/sayhello/SettingActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,6 +12,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class SettingActivity extends AppCompatActivity {
@@ -18,6 +20,9 @@ public class SettingActivity extends AppCompatActivity {
     private static final String TAG = "UserData";
     private FirebaseAuth firebaseAuth;
     private DatabaseReference userReference;
+    private TextView settingUsernameTextView;
+    private TextView settingEmailTextView;
+    private TextView settingPhoneNumberText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,30 +33,42 @@ public class SettingActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Setting");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // View Field
+        settingUsernameTextView = findViewById(R.id.userListUsernameTextView);
+        settingEmailTextView = findViewById(R.id.userListEmailTextView);
+        settingPhoneNumberText = findViewById(R.id.settingPhoneNumberText);
+
         // Firebase Auth
         firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
         // Get Current User
         FirebaseUser user = firebaseAuth.getCurrentUser();
+
         if (user != null) {
             String uid = user.getUid();
-            userReference = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
-            if (userReference != null) {
-                userReference.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        String username = dataSnapshot.child("username").getValue(String.class);
-                        String phoneNumber = dataSnapshot.child("phoneNumber").getValue(String.class);
-                    }
+            Query query = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+            query.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Log.d("dataSnap", "Value" + dataSnapshot);
+                    String username = dataSnapshot.child("username").getValue(String.class);
+                    String email = dataSnapshot.child("email").getValue(String.class);
+                    String phoneNumber = dataSnapshot.child("phoneNumber").getValue(String.class);
+//                    settingUsernameTextView.setText(username);
+//                        settingEmailTextView.setText(email);
+//                        settingPhoneNumberText.setText(phoneNumber);
+                }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        Log.e(TAG, "Error:" + databaseError.toException());
-                    }
-                });
-            }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
         }
-
-
     }
 }
